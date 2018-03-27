8a5b8ef3f5ca9183524ec1d1d7ebc428c2904598
Segger <saintdane@gmail.com>
2018-03-27 10:18:20 -0400

    Whitespace alignment
    random whitespace cleanup I guess idk

--- plugins/glumNet.js
+++ plugins/glumNet.js
@@ -1,23 +1,24 @@
 /*
-								glumNet.js
+					 glumNet.js
 		"The net's foremost contender in all things gloomy and glum!"
 
 		glumNet hosts a web display for users to see those who are banned
-		as well as anything else that needs displaying from the 'public' dir
+		as well as anything else that needs displaying from the 'public' dir.
 */
 var express = require('express');
-var app = express();	 									//Make express instance
-app.use(express.static(__dirname + './../public/'));		//Static file routing
-var port = 8080;			 								//Define port
-var server = app.listen(port,function(){					//Start server instance from express.
+var app = express();	 								//Make express instance
+app.use(express.static(__dirname + './../public/'));					//Static file routing
+var port = 8080;			 						//Define port
+var server = app.listen(port,function(){						//Start server instance from express.
 	console.log("glumNet web server started on port",port);
 });
 
 
 /*
-	Setup socket.io for communication
+	Setup socket.io for web
+	client communication
 */
-var sPort = 9001 											//Socket port to use for comms
+var sPort = 9001 									//Socket port to use for comms
 var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
